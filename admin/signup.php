eceb5fe81de57d38d7bac73411bacc80b8314648
lan1605 <phuonglantonhuynh@gmail.com>
2022-12-07 14:51:19 +0700

    em Lan update

--- admin/signup.php
+++ admin/signup.php
@@ -1,8 +1,10 @@
 <?php
 include("../layout/header.php");
 ?>
-<title>Đăng nhập vào Admin</title>
+<title>Đăng ký tài khoản</title>
+
 </head>
+
 <body class="bg-surface">
 
   <!--start wrapper-->
@@ -15,10 +17,10 @@ include("../layout/header.php");
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent" style="flex: 0 0 auto ">
+              <div class="collapse navbar-collapse" id="navbarSupportedContent" style="flex: 0 0 auto">
                 <div class="d-flex ms-3 gap-3">
-                  <a href="../admin/index.php" class="btn btn-primary btn-sm px-4 radius-30">Đăng nhập</a>
-                  <a href="../admin/signup.php" class="btn btn-white btn-sm px-4 radius-30">Đăng ký</a>
+                  <a href="../admin/index.php" class="btn btn-white btn-sm px-4 radius-30">Đăng nhập</a>
+                  <a href="../admin/signup.php" class="btn btn-primary btn-sm px-4 radius-30">Đăng ký</a>
                 </div>
               </div>
             </div>
@@ -29,39 +31,49 @@ include("../layout/header.php");
        <main class="authentication-content">
         <div class="container">
           <div class="mt-4">
-            <div class="card rounded-0 overflow-hidden shadow-none border mb-5 mb-lg-0">
+            <div class="card rounded-0 overflow-hidden shadow-none bg-white border">
               <div class="row g-0">
                 <div class="col-12 order-1 col-xl-8 d-flex align-items-center justify-content-center border-end">
-                  <img src="../assets/images/error/auth-img-7.png" class="img-fluid" alt="">
+                  <img src="../assets/images/error/auth-img-register3.png" class="img-fluid" alt="">
                 </div>
                 <div class="col-12 col-xl-4 order-xl-2">
                   <div class="card-body p-4 p-sm-5">
-                    <h5 class="card-title">Đăng nhập</h5>
+                    <h5 class="card-title">Đăng ký</h5>
                      <form class="form-body">
+                      
                         <div class="row g-3">
-                          <div class="col-12">
-                            <label for="inputEmailAddress" class="form-label">Địa chỉ email hoặc tên người dùng</label>
+                          <div class="col-12 ">
+                            <label for="inputName" class="form-label">Tên người dùng</label>
                             <div class="ms-auto position-relative">
-                              <input type="email" class="form-control radius-30" id="inputEmailAddress">
+                              <input type="email" class="form-control radius-30" id="inputName" >
                             </div>
                           </div>
                           <div class="col-12">
-                            <label for="inputChoosePassword" class="form-label">Mật khẩu</label>
+                            <label for="inputEmailAddress" class="form-label">Địa chỉ email</label>
                             <div class="ms-auto position-relative">
-                              <input type="password" class="form-control radius-30" id="inputChoosePassword">
+                              <input type="email" class="form-control radius-30" id="inputEmailAddress" >
+                            </div>
+                          </div>
+                          <div class="col-12">
+                            <label for="inputChoosePassword" class="form-label">Mật khẩu: </label>
+                            <div class="ms-auto position-relative">
+                              <input type="password" class="form-control radius-30" id="inputChoosePassword" >
+                            </div>
+                          </div>
+                          <div class="col-12">
+                            <label for="inputChoosePassword" class="form-label">Nhập lại mật khẩu: </label>
+                            <div class="ms-auto position-relative">
+                              <input type="password" class="form-control radius-30" id="inputChoosePassword" >
                             </div>
                           </div>
                           
-                          <div class="col-6 text-end">	<a href="authentication-forgot-password.html">Quên mật khẩu?</a>
-                          </div>
                           <div class="col-12">
                             <div class="d-grid">
-                              <button type="submit" class="btn btn-primary radius-30">Đăng nhập</button>
+                              <button type="submit" class="btn btn-primary radius-30">Đăng ký</button>
                             </div>
                           </div>
-                          
                           <div class="col-12 text-center">
-                            <p class="mb-0">Bạn chưa có tài khoản? <a href="authentication-signup-with-header-footer.html">Đăng ký tại đây</a></p>
+                            <p class="mb-0">Bạn đã có tài khoản? <a href="authentication-signin-with-header-footer.html">Đăng nhập tại đây</a></p>
                           </div>
                         </div>
                     </form>
@@ -75,8 +87,8 @@ include("../layout/header.php");
         
        <!--end page main-->
 
-       <footer class="bg-white border-top p-3 text-center fixed-bottom">
-        <p class="mb-0">© Minh Duy Solutions 2022. All right reserved.</p>
+       <footer class="bg-white border-top p-3 text-center">
+       <p class="mb-0">© Minh Duy Solutions 2022. All right reserved.</p>
       </footer>
 
   </div>
